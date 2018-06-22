1f707dcaec9b409a5f3024df162d9025f88f5c54
Anton Mitsengendler <a.mic@rtc.ru>
2018-06-22 10:53:46 +0300

    Add project files.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var m = new MyStruct();
+            m.a = 22;
+            using (m)
+            {
+                //m.a = 10;
+            }
+
+            Console.WriteLine(m.a);
+        }
+    }
+
+
+    struct MyStruct : IDisposable
+    {
+        public int a;
+
+        public void Dispose()
+        {
+
+            a = 5;
+        }
+    }
+}
