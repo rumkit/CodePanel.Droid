4c39b79d74de0feea8a52615636978414c846396
Anton Mitsengendler <a.mic@rtc.ru>
2018-06-22 13:25:26 +0300

    Ввод пароля с кнопок на экране Текст пароля все еще не по центру

---
+++ CodePanel/CodePanel.Android/CustomRenderer/PasswordEntryRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CodePanel;
+using CodePanel.Droid.CustomRenderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(PasswordEntry), typeof(PasswordEntryRenderer))]
+namespace CodePanel.Droid.CustomRenderer
+{
+    class PasswordEntryRenderer : EntryRenderer
+    {
+        public PasswordEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement == null) return;
+            var ctrl = (Entry)Element;
+            ctrl.HorizontalTextAlignment = Xamarin.Forms.TextAlignment.Center;
+        }
+
+        //public override void ChildDrawableStateChanged(Android.Views.View child)
+        //{
+        //    base.ChildDrawableStateChanged(child);
+        //    Control.Text = Control.Text;
+        //}
+    }
+}
--- CodePanel/CodePanel/MainPage.xaml.cs
+++ CodePanel/CodePanel/MainPage.xaml.cs
@@ -12,6 +12,42 @@ namespace CodePanel
 		public MainPage()
 		{
 			InitializeComponent();
+		    foreach (var child in PanelGrid.Children)
+		    {
+		        if (child is Button button)
+		            button.Clicked += CodePanelButton_Clicked;
+		    }
 		}
+
+	    private void CodePanelButton_Clicked(object sender, EventArgs e)
+	    {
+	        var button = sender as Button;
+            if(button == null)
+                return;
+            if(PasswordEntry.Text?.Length < PasswordLength)
+                PasswordEntry.Text += button.Text;
+	    }
+
+	    private void EraseButton_Clicked(object sender, EventArgs e)
+	    {
+	        PasswordEntry.Text = string.Empty;
+	    }
+
+        private const int PasswordLength = 5;
+
+	    private void PasswordEntry_OnTextChanged(object sender, TextChangedEventArgs e)
+	    {
+	        EraseButton.IsEnabled = e.NewTextValue.Length > 0;
+	        if (e.NewTextValue.Length == PasswordLength)
+	        {
+	            ConfirmButton.IsEnabled = true;
+                ConfirmButton.BackgroundColor = Color.Green;
+	        }
+	        else
+	        {
+	            ConfirmButton.IsEnabled = false;
+                ConfirmButton.BackgroundColor = Color.Gray;
+	        }
+	    }
 	}
 }
